54a9e4667ab8e093c5f808b38497cbffbed0319a
kartikgupta666 <kartikgangil@gmail.com>
2024-02-09 22:36:05 +0530

    .then and . catch include

--- mern-app/backend/db.js
+++ mern-app/backend/db.js
@@ -1,9 +1,13 @@
-const MongoClient = require ('mongoose');
+const MongoClient = require('mongoose');
 const mongoURI = 'mongodb+srv://gofood:123@cluster0.cahbfl3.mongodb.net/?retryWrites=true&w=majority';
 
 const connectToMongoDB = async () => {
-   await MongoClient.connect(mongoURI);
+    await MongoClient.connect(mongoURI).then(() => {
         console.log('Connected to MongoDB');
+
+    }).catch((e) => {
+        console.log(e.message);
+    });
 };
 
-module.exports =  connectToMongoDB;
+module.exports = connectToMongoDB;
