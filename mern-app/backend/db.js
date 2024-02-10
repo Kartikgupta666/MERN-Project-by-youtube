a2e0a12793d17793090bca2fc2f7bbe467c47fac
kartikgupta666 <kartikgangil@gmail.com>
2024-02-10 17:27:13 +0530

    fetch code is updated in db.js

--- mern-app/backend/db.js
+++ mern-app/backend/db.js
@@ -1,10 +1,18 @@
-const MongoClient = require('mongoose');
-const mongoURI = 'mongodb+srv://gofood:123@cluster0.cahbfl3.mongodb.net/?retryWrites=true&w=majority';
+const mongoose = require('mongoose');
+const mongoURI = 'mongodb+srv://gofood:123@cluster0.cahbfl3.mongodb.net/gofoodmern?retryWrites=true&w=majority';
 
 const connectToMongoDB = async () => {
-    await MongoClient.connect(mongoURI).then(() => {
+    await mongoose.connect(mongoURI).then(() => {
         console.log('Connected to MongoDB');
-
+        const fetched_data = mongoose.connection.db.collection("food_items");
+        fetched_data.find({}).toArray(function (err, data) {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                console.log(data);
+            }
+        })
     }).catch((e) => {
         console.log(e.message);
     });
--- mern-app/backend/index.js
+++ mern-app/backend/index.js
@@ -3,13 +3,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const  connectToMongoDB = require("./db");
- connectToMongoDB();
+const connectToMongoDB = require("./db");
+connectToMongoDB();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
